b5cbf786904f83608523924e20ad4180f7eb13df
Sebastian Hernandez <sebastian4hm@gmail.com>
2023-06-09 21:13:49 -0600

    Add quote

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css';
 import Calculator from './components/calculator';
+import Quote from './components/quote';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Calculator />
+    <Quote />
   </React.StrictMode>,
 );
